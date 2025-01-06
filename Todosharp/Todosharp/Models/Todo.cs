2087ccc38a956eab457e86f9bb2a43eee7dc0d28
frederic <flausson@normandiewebschool.fr>
2025-01-06 14:27:44 +0100

    fix model import

---
+++ Todosharp/Todosharp/Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿namespace Todosharp.Models;
+
+public class TodoModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public DateTime DueDate { get; set; }
+
+    public TodoStatus Status { get; set; } = TodoStatus.ToDo;
+}
+
+public enum TodoStatus
+{
+    ToDo,
+    InProgress,
+    Done
+}
